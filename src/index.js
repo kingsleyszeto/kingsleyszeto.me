b2d40c466343ce56db251e890a146a9aebeaabeb
Kingsley Szeto <kingsleyszeto@Kingsleys-MacBook-Pro.local>
2021-12-28 23:38:26 -0800

    revamp site

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oxygen" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans" />
+    <script src="https://unpkg.com/react/umd/react.production.min.js" crossOrigin />
+    <script
+      src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+      crossOrigin
+    />
+    <script
+      src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+      crossOrigin
+    />
+    <script>var Alert = ReactBootstrap.Alert;</script>
+
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
